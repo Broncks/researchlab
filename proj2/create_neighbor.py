ec08845bb2fe6c35e1b3db2f4be53ce2634fbc90
Thomas Pioch <t.pioch1990@gmail.com>
2021-05-05 19:49:06 +0200

    werteanpassung

--- proj2/create_neighbor.py
+++ proj2/create_neighbor.py
@@ -10,14 +10,15 @@ def create_neighbor_list(best_solution):
     for i in best_solution:
         if i >= 5:
             neighbor_list.append(i - 4)
-
         elif i == 0:
             neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.0, 0.50, 0.50]))
         elif i == 1:
             neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.50, 0.25, 0.25]))
         elif i == 2:
             neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.4, 0.4, 0.2]))
+        elif i == 3:
+            neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.5, 0.4, 0.1]))
         else:
 
-            neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.45, 0.45, 0.1]))
+            neighbor_list.append(i + np.random.choice([-1, 0, 1], p=[0.7, 0.2, 0.1]))
     return neighbor_list
