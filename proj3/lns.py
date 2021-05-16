adb97e55bed54b97072379b8a2d93065a4562e5a
NiklasRuge <niklas@ruge-hamburg.de>
2021-05-16 19:56:33 +0200

    lns

--- proj3/lns.py
+++ proj3/lns.py
@@ -1,5 +1,11 @@
+import numpy as np
+import math
+import random
 
-def lns(warehouse, rand_solutionlist):
+sa_temperature = 1000
+
+
+def lns(rmfs, rand_solutionlist, demandlist):
     iterations = 200
     
     solutionlist = rand_solutionlist
@@ -9,11 +15,12 @@ def lns(warehouse, rand_solutionlist):
 #   TODO: stop_criterion
     for i in range(iterations):
         candidate_solution = repair(destroy(solutionlist))
-        if accept(solutionlist, candidate_solution):
+
+        if accept(rmfs, demandlist, solutionlist, candidate_solution):
             solutionlist = candidate_solution
 
-        storage_sl, cost_sl = warehouse.run(solutionlist)
-        storage_best, cost_best = warehouse.run(best_solutionlist)
+        storage_sl, cost_sl = rmfs.run(solutionlist)
+        storage_best, cost_best = rmfs.run(best_solutionlist)
         if cost_sl < cost_best:
             best_solutionlist = solutionlist
 
@@ -21,11 +28,40 @@ def lns(warehouse, rand_solutionlist):
         
         
         
-def destroy(sl): # TODO
+def destroy(sl):
+    percentage = 15
+
+    for i in range(len(sl)):
+        if np.random.randint(100) <= percentage:
+            sl[i] = -1
+
     return sl
-    
+
+
 def repair(sl): # TODO
+
+    for i in range(len(sl)):
+        if sl[i] == -1:
+            sl[i] = np.random.choice([0, 1], p=[0.999, 0.001])
+
     return sl
     
-def accept(sl, sl_cand): # TODO
-    return True # or False
+def accept(rmfs, demandlist, sl, sl_cand): # TODO
+    global sa_temperature
+    cooling_factor = 0.9
+
+    # determine storage & cost based on solutionlists provided in method-argument
+    storage, current_cost = rmfs.run(demandlist, sl)
+    storage, neighbor_cost = rmfs.run(demandlist, sl_cand)
+    cost_difference = neighbor_cost - current_cost
+
+    if math.exp(-cost_difference / sa_temperature) > random.uniform(0, 1) or cost_difference < 0:
+        sa_temperature *= cooling_factor  # cool temperature down
+        return True
+    else:
+        sa_temperature *= cooling_factor  # cool temperature down
+        return False
+
+
+
+
